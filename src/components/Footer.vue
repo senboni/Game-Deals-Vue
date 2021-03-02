f458ac9bc503db1db241de277f9f02803da89e8a
mihajlobisevac <kajler11@gmail.com>
2021-03-02 20:39:14 +0100

    added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <footer>
+        <p>
+            Game Deals ©
+            <a href="https://apidocs.cheapshark.com/">CheapShark</a>
+            2021
+        </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+    display: flex;
+    justify-content: center;
+
+    background: var(--clr-dark__white);
+    margin: 2em 0 0 0;
+    padding: 4em 0;
+
+    color: var(--clr-light__gray);
+    font-size: 0.9rem;
+    font-weight: 400;
+    word-spacing: 0.25rem;
+}
+
+p {
+    display: inline-block;
+    margin: 0;
+}
+
+a {
+    color: var(--clr-gray);
+}
+
+a:hover {
+    color: var(--clr-purple);
+}
+</style>
