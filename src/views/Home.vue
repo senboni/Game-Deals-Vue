2ceffb400317b0de683ce9d38b7bdec5060edeba
mihajlobisevac <kajler11@gmail.com>
2021-03-03 03:07:51 +0100

    added axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,50 @@
 <template>
   <Title msg="Today's Deals"/>
 
-  <div v-for="deal in deals" :key="deal.id">
-    {{deal.name}} {{deal.id}}
+  <div v-if="loading === false">
+    <div v-for="deal in deals" :key="deal.dealID">
+      {{deal.title}} {{deal.normalPrice}} {{deal.salePrice}}
+    </div>
+  </div>
+  <div v-else>
+    Loading...
   </div>
 </template>
 
 <script>
 import Title from '@/components/shared/Title.vue'
+import axios from 'axios'
 
 export default {
-  name: 'Home',
-  components: {
-    Title
-  },
-  data() {
-    return {
-      deals: [
-        {
-          id: 1,
-          name: "deald"
-        },
-        {
-          id: 2,
-          name: "dealdd"
-        },
-        {
-          id: 3,
-          name: "dealaa"
-        },
-      ]
+    name: 'Home',
+    components: {
+        Title
+    },
+    data() {
+      return {
+        loading: false,
+        deals: []
+      }
+    },
+    mounted() {
+      this.getGameDeals()
+    },
+    methods: {
+      getGameDeals() {
+        this.loading = true
+
+        axios.get(`https://www.cheapshark.com/api/1.0/deals?pageNumber=1&pageSize=3`)
+          .then(res => {
+              this.deals = res.data
+          })
+          .catch(err => {
+              console.log(err)
+          })
+          .then(() => {
+              this.loading = false
+          })
+      }
     }
-  }
 }
 </script>
 
