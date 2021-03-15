e25a0fddaefb6e34206d6615478e230fbea00762
mihajlobisevac <kajler11@gmail.com>
2021-03-15 03:32:03 +0100

    GameInfo component setup

---
+++ src/components/games/GameInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  	<div>{{game.cheapest}}</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchBar',
+	props: {
+		game: Object
+	},
+	data() {
+		return {
+		}
+	},
+	methods: {
+	},
+	computed: {
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,23 +1,50 @@
 <template>
 	<SearchBar @submit-search="submitSearch" />
+	<GameInfo 
+		v-for="game in games"
+		:key="game.gameID"
+		:game="game"
+	/>
 </template>
 
 <script>
+import axios from 'axios'
 import SearchBar from '@/components/games/SearchBar'
+import GameInfo from '@/components/games/GameInfo'
 
 export default {
 	name: 'Games',
 	components: {
-		SearchBar
+		SearchBar,
+		GameInfo
 	},
 	data() {
 		return {
-			searchInput: ''
+			loading: false,
+			searchInput: "reddeadred",
+			games: null
 		}
 	},
+	mounted() {
+		this.getGames(this.searchInput)
+	},
 	methods: {
-		submitSearch() {
-			console.log('no')
+		getGames(input) {
+			this.loading = true
+
+			axios.get(`https://www.cheapshark.com/api/1.0/games?title=${input}`)
+				.then(res => {
+					this.games = res.data
+				})
+				.catch(err => {
+					console.log(err)
+				})
+				.then(() => {
+					this.loading = false
+				})
+		},
+		submitSearch(input) {
+			this.getGames(input)
 		}
 	},
 	computed: {
